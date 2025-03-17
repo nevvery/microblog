0ad4af4bda17197d385ed3b37e81a021fd49e22f
nevvery <nerkk953@gmail.com>
2025-03-17 22:45:14 +0300

    init commit

---
+++ app/__init__.py
@@ -0,0 +1,26 @@
+import os
+
+from flask import Flask
+from Config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+db = SQLAlchemy()
+migrate = Migrate()
+basedir = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
+template_folder = os.path.join(basedir, 'templates')
+
+def create_app(config_name=Config):
+    app = Flask(__name__, template_folder=template_folder)
+    app.config.from_object(config_name)
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+    from app.main import bp as main_bp
+    app.register_blueprint(main_bp)
+
+    from app.auth import bp as auth_bp
+    app.register_blueprint(auth_bp, url_prefix='/auth')
+
+    return app
---
+++ app/auth/routes.py
@@ -0,0 +1,7 @@
+from flask import render_template
+from app.auth import bp
+
+
+@bp.route('/login')
+def login():
+    pass
