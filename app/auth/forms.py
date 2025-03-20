11d0cc9ca6d43dd0ee816b25ede418e0bf94c847
nevvery <nerkk953@gmail.com>
2025-03-20 22:32:43 +0300

    make register\logout

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,7 +1,11 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, BooleanField
 
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Email, ValidationError, EqualTo
+import sqlalchemy as sa
+
+from app.models import User
+from app import db
 
 
 class LoginForm(FlaskForm):
@@ -9,3 +13,23 @@ class LoginForm(FlaskForm):
     password = StringField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember Me')
     submit = SubmitField('Sign In')
+
+
+class RegisterForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = StringField('Password', validators=[DataRequired()])
+    password2 = StringField('<PASSWORD>', validators=[DataRequired(), EqualTo('password')])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    submit = SubmitField('Register')
+
+    def validate_username(self, username):
+        user = db.session.scalar(sa.select(User).where(User.username == username.data))
+
+        if user is not None:
+            raise ValidationError('Username already in use.')
+
+    def validate_email(self, email):
+        user = db.session.scalar(sa.select(User).where(User.email == email.data))
+
+        if user is not None:
+            raise ValidationError('Email already in use.')
--- app/auth/routes.py
+++ app/auth/routes.py
@@ -1,14 +1,18 @@
 from flask import render_template, redirect, url_for, flash
+
 from app.auth import bp
-from app.auth.forms import LoginForm
+from app.auth.forms import LoginForm, RegisterForm
 from app.models import User
 from app import db
 import sqlalchemy as sa
-from flask_login import login_user
+from flask_login import login_user, current_user, logout_user
 
 
 @bp.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('main.index'))
+
     form = LoginForm()
 
     if form.validate_on_submit():
@@ -22,3 +26,29 @@ def login():
         return redirect(url_for('main.index'))
 
     return render_template('auth/login.html', title='Login', form=form)
+
+
+@bp.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('main.index'))
+
+    form = RegisterForm()
+
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+
+        db.session.add(user)
+        db.session.commit()
+
+        flash('Thank you for registering. You can now login.')
+        return redirect(url_for('auth.login'))
+
+    return render_template('auth/register.html', title='Register', form=form)
+
+
+@bp.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('main.index'))
